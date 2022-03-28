7322e3823348bcf8a34803ff3c65b403d3887038
Alan Poveda <alsepoveda@gmail.com>
2022-03-28 20:12:14 -0300

    Enum examples

--- lib/sumlist.ex
+++ lib/sumlist.ex
@@ -3,6 +3,11 @@ defmodule Sumlist do
   #e sempre começar o contador com zero no caso
   def call(list), do: sum(list,0)
 
+  # Enum
+  def call_enum(list), do: Enum.sum(list)
+  def call_enum_max(list), do: Enum.max(list)
+  def call_enum_min(list), do: Enum.min(list)
+  def call_enum_map(list), do: Enum.map(list, fn elem -> elem + 1 end)
   #Vai chegar um momento que vai zerar a lista, então para não dar erro, é necessário essa função
   defp sum([],acc), do: acc
 
